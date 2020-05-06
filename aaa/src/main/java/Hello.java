cdc47426b372f7143e2334ab28c6d8090ca19446
li1106330636 <1106330636@qq.com>
2020-05-06 22:45:53 +0800

    Initial commit

---
+++ aaa/src/main/java/Hello.java
@@ -0,0 +1,8 @@
+import static sun.misc.Version.print;
+import static sun.misc.Version.println;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
